c657376f5a09a2fdb640f4d21c2c21786f3a56e2
Nachiketh <nachikethyadavgmailcom>
2026-07-11 22:55:44 +0530

    Switch to HashRouter for GitHub Pages deployment compatibility

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom' // Ensure this is installed
+import { HashRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* Add the basename here if you are using React Router */}
-    <BrowserRouter basename="/Disney-Hotstar-react/">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
