8a9e6627abda348a879cd478bbbbcbd2af906f61
jayashree <jayashreem923@email.com>
2026-07-11 20:27:22 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom' // Ensure this is installed
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* Add the basename here if you are using React Router */}
+    <BrowserRouter basename="/DISNEY+HOTSTAR/">
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
