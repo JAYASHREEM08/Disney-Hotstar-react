cc62e98ffd3745821c237ce0cc393a2f41146dee
Varshinishree <varshinishreekannan2006@gmail.com>
2026-07-11 23:03:11 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './home.jsx/Home';
 import Profile from './profile/Profile';
-import Search from './search/Search';
 import './home.jsx/style.css';
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/profile" element={<Profile />} />
-      <Route path="/search" element={<Search />} />
     </Routes>
   );
 }
