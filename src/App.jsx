6d81882e0c9687bb048f04ae68e64d065e2efcd3
JAYASHREEM08 <jayashreem923@gmail.com>
2026-07-26 20:58:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './home.jsx/Home';
 import Profile from './profile/Profile';
+import Search from "./search/Search"; 
 import './home.jsx/style.css';
 
 function App() {
